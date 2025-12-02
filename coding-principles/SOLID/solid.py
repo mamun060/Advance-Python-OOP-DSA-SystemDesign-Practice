9958a979bd6e078e37513d10c9e21671c31f36d1
mamun060 <almamun.raju520@gmail.com>
2025-12-02 18:01:34 +0600

    solid practice

--- coding-principles/SOLID/solid.py
+++ coding-principles/SOLID/solid.py
@@ -1,16 +1,79 @@
-# SOLID Principles Example
+from abc import ABC, abstractmethod
+# S
+class UserRepo:
+    def create(self, data):
+        print("User created")
 
-# Single Responsibility Principle - only one reason to change
-class University:
-    def __init_(self, name, location):
-        self.name = name 
-        self.location = location 
+class EmailService:
+    def send(self, email):
+        print("Email sent")
 
+class Logger:
+    def log(self, msg):
+        print("Log:", msg)
 
-# Open/close principle - open for extension, closed for modification - I mean we can add new features without changeing exiting code.
-class Course:
-    def __init__(self, title, credits):
-        self.title = title
-        self.credits = credits
-    
-    def includeCourse(self)
+# O
+class Discount(ABC):
+    @abstractmethod
+    def apply(self, amount):
+        pass
+
+class PercentageDiscount(Discount):
+    def apply(self, amount):
+        return amount * 0.90
+
+class FlatDiscount(Discount):
+    def apply(self, amount):
+        return amount - 100
+
+# L
+class Bird:
+    pass
+
+class FlyingBird(Bird):
+    def fly(self):
+        print("Flying")
+
+class Ostrich(Bird):
+    def run(self):
+        print("Running")
+
+# I
+class Workable(ABC):
+    @abstractmethod
+    def work(self): pass
+
+class Eatable(ABC):
+    @abstractmethod
+    def eat(self): pass
+
+class Robot(Workable):
+    def work(self):
+        print("Robot working")
+
+class Human(Workable, Eatable):
+    def work(self):
+        print("Human working")
+    def eat(self):
+        print("Human eating")
+
+# D
+class InputDevice(ABC):
+    @abstractmethod
+    def press(self):
+        pass
+
+class Keyboard(InputDevice):
+    def press(self):
+        print("Keyboard pressed")
+
+class Mouse(InputDevice):
+    def press(self):
+        print("Mouse clicked")
+
+class Computer:
+    def __init__(self, device: InputDevice):
+        self.device = device
+
+pc = Computer(Keyboard())
+pc = Computer(Mouse())
